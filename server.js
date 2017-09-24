44fe6d5d6414e8ea07217ea876ee869ac76c142d
Gobinath <gopinathr2010@gmail.com>
2017-09-24 22:13:49 +0530

    added user and applied css

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express= require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+
 var app= express();
+var requiredPath=path.join(__dirname,'./public/');
+
+app.use(express.static(requiredPath));
+app.use(bodyParser.json())
+
 const port=process.env.PORT||3000;
 
 
