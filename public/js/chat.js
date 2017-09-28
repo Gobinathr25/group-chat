3f89d5ca7ff7106a80c5099c81b8cb52925a95f6
Gobinath <gopinathr2010@gmail.com>
2017-09-28 07:01:20 +0530

    gitrepo

---
+++ public/js/chat.js
@@ -0,0 +1,15 @@
+
+var socket=io("http://localhost:4000");
+
+// socket.on('connect',()=>{
+//     console.log('A new user connected to server')
+// })
+
+
+
+// function getUsername(){
+//     var socket=io();
+//     alert('started')
+//     var user=document.getElementById('userId').value;
+//     socket.emit('newUser',user);    
+// }
--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express= require('express');
 const path=require('path');
 const bodyParser=require('body-parser');
+const socketIO=require('socket.io');
+const http=require('http');
+var server=http.createServer(app);
+var io= socketIO(server);
 
 var app= express();
-var requiredPath=path.join(__dirname,'./public/');
+ var users=[];
 
-app.use(express.static(requiredPath));
-app.use(bodyParser.json())
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json());
 
-const port=process.env.PORT||3000;
+//app.engine('html',require('ejs').renderFile);
+
+// app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
+io.on('connection',(socket)=>{
+    console.log("A new user connected to Client");
+})
+app.get('/chat',(req,res)=>{
+    res.render('joinuser.html');
+})
+
+
+
+
+
+
+const port=process.env.PORT||4000;
 
 
 app.listen(port,()=>{
