226d2175a0250963f9f20e2650f9884ff053f2cb
Devi <devi.balasubramanian@photoninfotech.net>
2017-09-28 14:43:35 +0530

    Initial commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,77 @@
+/*
 
-var socket=io("http://localhost:4000");
+var socket = io.connect("http://localhost:4000");
 
-// socket.on('connect',()=>{
-//     console.log('A new user connected to server')
-// })
+socket.on('connect',()=>{
+    console.log('A new user connected to server')
+})
+
+socket.on('showName',(username) =>{
+    console.log(username,"SERN MESSAGE")
+})
+
+function getUsername(){
+    var socket=io();
+    alert('started')
+    var user= document.getElementById('userId').value;
+    console.log(user)
+    socket.emit('addUser',user);
+    window.location.href = '/sendMsg';
+
+/!*    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:4000/sendMsg',
+        data: {
+            user: user
+        },
+        success: function (result) {
+            console.log(" :::::::RESULT ::::",result)
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });*!/
+
+}*/
 
 
+var name,
+    socket = io.connect("http://localhost:4000");
+$(function () {
+    //as the user to enter their nick name or name.
+    name = window.prompt("enter your name");
+    //If the name is not given, ask the user to enter once again
+    if (name == null) {
+        $("body").html(" please refresh the page and try again ");
+    }
+    //When send button is clicked on, send the message to server
+    $("#send").click(function () {
+        //send to the server with person name and message
+        socket.emit("clientMsg", {
+            "name": name,
+            "msg": $("#msg").val()
+        });
+    });
 
-// function getUsername(){
-//     var socket=io();
-//     alert('started')
-//     var user=document.getElementById('userId').value;
-//     socket.emit('newUser',user);    
-// }
+    //After sending message to the server, we'll have to wire up the event for it.
+    //We can do the following. Upon receiving the message print it to the message box
+    //that we've created in our html
+    socket.on("serverMsg", function (data) {
+        //Append the message from the server to the message box
+        $("#msgBox").append("<strong>" + data.name +
+            "</strong>: " + data.msg + "<br/>");
+    });
+
+    $("#msg").on("keyup", function (event) {
+        socket.emit("sender", {
+            name: name
+        });
+    });
+
+    socket.on("sender", function (data) {
+        $("#status").html(data.name + " is typing");
+        setTimeout(function () {
+            $("#status").html('');
+        }, 3000);
+    });
+});
--- server.js
+++ server.js
@@ -1,39 +1,85 @@
+// express server initialization
 const express= require('express');
 const path=require('path');
 const bodyParser=require('body-parser');
+var app= express();
+const users=[],ids=[];
+
+ // socket.io initialization
 const socketIO=require('socket.io');
 const http=require('http');
 var server=http.createServer(app);
-var io= socketIO(server);
+var io= socketIO.listen(server);
 
-var app= express();
- var users=[];
-
+// To render html files,.etc
 app.use(express.static(__dirname+'/public'));
+app.engine('html', require('ejs').renderFile);
+
+// Body parser initialization
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json());
 
-//app.engine('html',require('ejs').renderFile);
 
-// app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
-app.engine('html', require('ejs').renderFile);
-io.on('connection',(socket)=>{
+// Socket server listening
+io.on('connection',(socket)=> {
     console.log("A new user connected to Client");
+
+ /*   socket.on('setUsername', function(data) {
+        console.log(data);
+
+        if(users.indexOf(data) > -1) {
+            socket.emit('userExists', data + ' username is taken! Try some other username.');
+        } else {
+            users.push(data);
+            socket.emit('userSet', {username: data});
+        }
+    });
+
+
+        socket.on('msg', function(data) {
+            //Send message to everyone
+            io.sockets.emit('newmsg', data);
+        })*/
+        //when receiving the data from the server, push the same message to client.
+        socket.on("clientMsg", function (data) {
+            //send the data to the current client requested/sent.
+            socket.emit("serverMsg", data);
+            //send the data to all the clients who are accessing the same site(localhost)
+            socket.broadcast.emit("serverMsg", data);
+        });
+
+        socket.on("sender", function (data) {
+            socket.emit("sender", data);
+            socket.broadcast.emit("sender", data);
+        });
+
+        socket.on('disconnect', () =>{
+        console.log('user disconnected');
+        });
 })
+
+
+// Router initiatization
 app.get('/chat',(req,res)=>{
     res.render('joinuser.html');
 })
 
 
+app.get('/sendMsg',(req,res)=>{
+    res.render('chat.html');
+})
+/*var express = require('express');
+
+var app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.listen(8080);*/
 
 
-
-
+// Main server listening ......
 const port=process.env.PORT||4000;
 
-
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('Server Intialiazed at',port)
 });
